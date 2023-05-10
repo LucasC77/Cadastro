11139869135589d153c7d676b3c4fce13a600d18
Lucas Cotrim <131214248+LucasC77@users.noreply.github.com>
2023-05-10 17:47:11 -0300

    Add files via upload

---
+++ Cadastros.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cadastross
+{
+    public partial class EntradaIHM : Form
+    {
+        public EntradaIHM()
+        {
+            InitializeComponent();
+        }
+
+        private void cadastroToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CadastroProdutos cadastros = new CadastroProdutos();
+            cadastros.Show();
+        }
+
+        private void cadastroAlunosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CadastroAlunos cadastro = new CadastroAlunos();
+            cadastro.Show();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
